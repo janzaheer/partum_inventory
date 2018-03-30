364e6f86891eacb9d027adce884eba6034a97b82
Asfand Yar <asfandyar@ubuntu.ubuntu-domain>
2018-03-30 16:27:46 +0500

    urls and views file added

--- pis_com/views.py
+++ pis_com/views.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+
+
+class HomePageView(TemplateView):
+    template_name = 'index.html'
