f5c1b755819316e61dda7f5bac04838d6f4b04aa
Zaheer Jan <zaheerjan0@gmail.com>
2018-04-12 17:17:19 +0500

    Create Invoice View and URL

--- pis_sales/urls.py
+++ pis_sales/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url, include
 
-from pis_sales.views import CreateBillingView, ProductItemAPIView
+from pis_sales.views import (
+    CreateBillingView, ProductItemAPIView, CreateInvoiceView,
+    InvoiceDetailView
+)
 
 urlpatterns = [
     url(r'^bill/create/$', CreateBillingView.as_view(), name='bill_create'),
@@ -9,4 +12,11 @@ urlpatterns = [
         ProductItemAPIView.as_view(),
         name='product_item_api'
     ),
+    url(
+        r'^create/invoice/$',
+        CreateInvoiceView.as_view(),
+        name='create_invoice'
+    ),
+    # TODO: This url needs invoice ID to show the invoice detail
+    url(r'^invoice/detail/$', InvoiceDetailView.as_view(), name='invoice_detail'),
 ]
--- pis_sales/views.py
+++ pis_sales/views.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.db.models import Sum
 from django.http import JsonResponse, HttpResponse, Http404
 from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import FormView, View
+from django.views.generic import FormView, View, TemplateView
 
 from pis_sales.forms import BillingForm
 
@@ -56,3 +56,18 @@ class ProductItemAPIView(View):
             items.append(p)
 
         return JsonResponse({'products': items})
+
+
+class CreateInvoiceView(View):
+
+    @csrf_exempt
+    def dispatch(self, request, *args, **kwargs):
+        return super(
+            CreateInvoiceView, self).dispatch(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return JsonResponse(request.POST)
+
+
+class InvoiceDetailView(TemplateView):
+    template_name = 'sales/invoice_detail.html'
