eccc1299f44fcb5b2658a8e912147b0ad257d5ef
Zaheer Jan <zaheerjan0@gmail.com>
2018-04-12 17:16:01 +0500

    update sales history models and its migration files

--- pis_sales/models.py
+++ pis_sales/models.py
@@ -9,30 +9,45 @@ class SalesHistory(DatedModel):
     retailer = models.ForeignKey(
         'pis_retailer.Retailer', related_name='retailer_sales'
     )
-    receipt_no = models.CharField(max_length=20)
+    receipt_no = models.CharField(
+        max_length=20, unique=True, blank=True, null=True
+    )
+
     customer_name = models.CharField(
         max_length=100, blank=True, null=True
     )
+
     customer_phone = models.CharField(
         max_length=20, null=True, blank=True
     )
+
     product_details = models.TextField(
         max_length=512, blank=True, null=True,
         help_text='Quantity and Product name would save in JSON format')
-    retail_amount = models.DecimalField(
-        max_digits=8, decimal_places=2, default=0
+
+    purchased_items = models.ManyToManyField(
+        'pis_product.PurchasedProduct',
+        max_length=100, blank=True, null=True
     )
-    consumer_amount = models.DecimalField(
-        max_digits=8, decimal_places=2, default=0
+
+    total_quantity = models.CharField(
+        max_length=10, blank=True, null=True, default=1)
+
+    sub_total = models.DecimalField(
+        max_digits=8, decimal_places=2, default=0, blank=True, null=True
     )
-    total = models.DecimalField(
-        max_digits=8, decimal_places=2, default=0
-    )
+
     discount = models.DecimalField(
-        max_digits=8, decimal_places=2, default=0
-    )
-    profit = models.DecimalField(
-        max_digits=8, decimal_places=2, default=0
+        max_digits=8, decimal_places=2, default=0, blank=True, null=True
     )
 
+    shipping = models.DecimalField(
+        max_digits=8, decimal_places=2, default=0, blank=True, null=True
+    )
 
+    grand_total = models.DecimalField(
+        max_digits=8, decimal_places=2, default=0, blank=True, null=True
+    )
+
+    def __unicode__(self):
+        return self.retailer.name
