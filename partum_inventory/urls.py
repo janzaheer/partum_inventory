2c502773c643adbb60c227bfb6ba8625741816cd
Hasnan <hasnan@ubuntu.ubuntu-domain>
2018-04-25 08:04:27 -0700

    validation in add product and add product item

--- partum_inventory/urls.py
+++ partum_inventory/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('pis_com.urls')),
     url(r'^product/', include('pis_product.urls', namespace='product')),
+    url(r'^retailer/', include('pis_retailer.urls', namespace='retailer')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- pis_retailer/views.py
+++ pis_retailer/views.py
@@ -1,6 +1,27 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.views.generic import View
+from django.http import JsonResponse
 
-from django.shortcuts import render
 
-# Create your views here.
+class RetailerProductsAPI(View):
+    """
+    API URL => {% url 'retailer:retailer_products' retailer_id=request.user.retailer_user.retailer.id %}
+    """
+
+    def get(self, requst, *args, **kwargs):
+        retailer = self.request.user.retailer_user.retailer
+        products = retailer.retailer_product.all()
+        retailer_products = {}
+        products_list = []
+
+        for product in products:
+            products_list.append({
+                'product_name': product.name,
+                'brand_name': product.brand_name,
+                'retailer': retailer.id
+            })
+
+        return JsonResponse(
+            {'retailer_products': products_list}
+        )
