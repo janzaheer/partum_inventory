a6d96384ec7db85a1b48a06d993a6b9346905a7a
Hasnan <hasnan@ubuntu.ubuntu-domain>
2018-04-29 07:19:16 -0700

    update the view doc string, remove extra code

--- pis_retailer/views.py
+++ pis_retailer/views.py
@@ -6,13 +6,17 @@ from django.http import JsonResponse
 
 class RetailerProductsAPI(View):
     """
-    API URL => {% url 'retailer:retailer_products' retailer_id=request.user.retailer_user.retailer.id %}
+    API URL for Developers => {%
+        url 'retailer:retailer_products'
+        retailer_id=request.user.retailer_user.retailer.id
+    %}
+    URL Endpoint => /retailer/1/products/
+    this API returns the retailer product details in json format
     """
 
     def get(self, requst, *args, **kwargs):
         retailer = self.request.user.retailer_user.retailer
         products = retailer.retailer_product.all()
-        retailer_products = {}
         products_list = []
 
         for product in products:
