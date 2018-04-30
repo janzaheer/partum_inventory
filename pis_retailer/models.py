4f7b1b21ffe79fb8bebae349cf2b484cbfb41135
partum <partum@ubuntu.(none)>
2018-04-30 19:31:35 +0500

    role field added in retailer user

--- pis_retailer/models.py
+++ pis_retailer/models.py
@@ -18,8 +18,23 @@ class Retailer(DatedModel):
 
 
 class RetailerUser(models.Model):
+    ROLE_TYPE_OWNER = 'owner'
+    ROLE_TYPE_DATA_ENTRY_USER = 'data_entry_user'
+    ROLE_TYPE_SALESMAN = 'salesman'
+    ROLE_TYPE_VIEW_ACCOUNT = 'account_viewer'
+
+    ROLE_TYPES = (
+        (ROLE_TYPE_OWNER, 'Owner'),
+        (ROLE_TYPE_DATA_ENTRY_USER, 'Data Entry User'),
+        (ROLE_TYPE_SALESMAN, 'Salesman'),
+        (ROLE_TYPE_VIEW_ACCOUNT, 'Account Viewer'),
+    )
+
     user = models.OneToOneField(User, related_name='retailer_user')
     retailer = models.ForeignKey(Retailer, related_name='u_retailer')
+    role_type = models.CharField(
+        max_length=100, choices=ROLE_TYPES, default=ROLE_TYPE_OWNER
+    )
 
     def __unicode__(self):
         return self.user.username
