bda4aca6fe2ed580385399e13c2341a4986c7fc8
Zaheer Jan <zaheerjan0@gmail.com>
2018-06-04 22:48:02 +0500

    customer urls

--- pis_com/urls.py
+++ pis_com/urls.py
@@ -4,10 +4,23 @@ from django.conf.urls import url, include
 from pis_com.views import HomePageView
 from pis_com.views import LoginView
 from pis_com.views import LogoutView
+from pis_com.views import CreateCustomer, CustomerTemplateView
 
 urlpatterns = [
     url(r'^$', HomePageView.as_view(), name='index'),
     url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^logout/$', LogoutView.as_view(), name='logout'),
+
+    url(
+        r'^customer/create/$',
+        CreateCustomer.as_view(),
+        name='create_customer'
+    ),
+
+url(
+        r'^customers/$',
+        CustomerTemplateView.as_view(),
+        name='customers'
+    ),
 ]
 
