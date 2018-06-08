9580e6f29b04d975477a9a92f0f417762514dc7a
Zaheer Jan <zaheerjan0@gmail.com>
2018-06-08 23:14:31 +0500

    views and template changes to do payment and update the purchased items value accordingly

--- pis_product/views.py
+++ pis_product/views.py
@@ -11,6 +11,7 @@ from django.http import Http404
 from pis_product.models import PurchasedProduct, ExtraItems
 from pis_product.forms import (
     ProductForm, ProductDetailsForm, ClaimedProductForm)
+from pis_ledger.forms import PaymentForm
 
 
 class ProductItemList(TemplateView):
@@ -165,8 +166,45 @@ class ClaimedProductFormView(FormView):
     template_name = 'products/claimed_product.html'
     form_class = ClaimedProductForm
 
+    @staticmethod
+    def purchased_items_update(claimed_item, claimed_number):
+        product = (
+            claimed_item.product.product_detail.filter(
+                available_item__gte=claimed_number
+            ).first()
+        )
+        product.purchased_item = (
+            product.purchased_item - claimed_number
+        )
+        product.save()
+
+    def claimed_items_payment(self, claimed_item, amount):
+        payment_form_kwargs = {
+            'customer': claimed_item.customer.id,
+            'retailer': self.request.user.retailer_user.retailer.id,
+            'amount': amount,
+            'description': 'Amount Refunded from Claimed'
+                           ' Item ID (%s)' % claimed_item.id
+        }
+        payment_form = PaymentForm(payment_form_kwargs)
+        if payment_form.is_valid():
+            payment_form.save()
+
     def form_valid(self, form):
-        form.save()
+        claimed_item = form.save()
+
+        # update the purchased product accordingly
+        self.purchased_items_update(
+            claimed_item=claimed_item,
+            claimed_number=int(form.cleaned_data.get('claimed_items'))
+        )
+
+        # Doing a payment of claimed amount
+        self.claimed_items_payment(
+            claimed_item=claimed_item,
+            amount=form.cleaned_data.get('claimed_amount')
+        )
+
         return HttpResponseRedirect(reverse('product:items_list'))
     
     def form_invalid(self, form):
@@ -178,10 +216,15 @@ class ClaimedProductFormView(FormView):
 
         products = (
             self.request.user.retailer_user.retailer.
-            retailer_product.all())
-
+            retailer_product.all().order_by('name')
+        )
+        customers = (
+            self.request.user.retailer_user.retailer.
+            retailer_customer.all().order_by('customer_name')
+        )
         context.update({
-            'products': products
+            'products': products,
+            'customers': customers,
         })
 
         return context
