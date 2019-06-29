ee1f6afa75b18ab8e7d5683dd9a9c0e42ece4d9d
Zaheer Jan <zaheerjan0@gmail.com>
2019-06-29 16:44:35 +0500

    sales list view added pagination

--- pis_sales/views.py
+++ pis_sales/views.py
@@ -7,7 +7,7 @@ from django.db.models import Sum
 from django.http import JsonResponse, HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt, csrf_protect
 from django.utils.decorators import method_decorator
-from django.views.generic import FormView, View, TemplateView
+from django.views.generic import FormView, View, TemplateView, ListView
 from django.utils import timezone
 from django.core.urlresolvers import reverse
 
@@ -252,13 +252,22 @@ class InvoiceDetailView(TemplateView):
         return context
 
 
-class InvoicesList(TemplateView):
+class InvoicesList(ListView):
     template_name = 'sales/invoice_list.html'
+    model = SalesHistory
+    paginate_by = 200
     
     def dispatch(self, request, *args, **kwargs):
         if not self.request.user.is_authenticated():
             return HttpResponseRedirect(reverse('login'))
         return super(InvoicesList, self).dispatch(request, *args, **kwargs)
+
+    def get_queryset(self):
+        queryset = (
+            self.request.user.retailer_user.retailer.
+            retailer_sales.all().order_by('-created_at')
+        )
+        return queryset
 
     def get_sales_history(self):
         return (
@@ -268,9 +277,6 @@ class InvoicesList(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(InvoicesList, self).get_context_data(**kwargs)
-        context.update({
-            'invoices': self.get_sales_history(),
-        })
         return context
 
 
