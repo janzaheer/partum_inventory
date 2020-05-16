2068ded5ec11c7712174670909b730e35522ba0f
Milton Medina C <miltonmce@gmail.com>
2020-05-15 19:42:51 -0700

    static files works

--- partum_inventory/settings.py
+++ partum_inventory/settings.py
@@ -124,8 +124,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_URL = os.path.join(BASE_DIR, 'app_static/')
+STATIC_URL = '/app_static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "app_static"),
+    '/var/www/static/',
+]
+
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root/')
 
